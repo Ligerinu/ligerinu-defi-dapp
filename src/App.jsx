47c9bf89d76b0dbc0e8313c73d6e900559e17d38
tiger <metarealityprivatelimited@Metarealitys-MacBook-Air.local>
2022-02-15 17:48:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,185 @@
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Redirect,
+} from "react-router-dom";
+import Account from "components/Account/Account";
+import Chains from "components/Chains";
+import TokenPrice from "components/TokenPrice";
+import ERC20Balance from "components/ERC20Balance";
+import ERC20Transfers from "components/ERC20Transfers";
+import DEX from "components/DEX";
+import NFTBalance from "components/NFTBalance";
+import Wallet from "components/Wallet";
+import { Layout, Tabs } from "antd";
+import "antd/dist/antd.css";
+import NativeBalance from "components/NativeBalance";
+import "./style.css";
+import QuickStart from "components/QuickStart";
+import Contract from "components/Contract/Contract";
+import Text from "antd/lib/typography/Text";
+import Ramper from "components/Ramper";
+import MenuItems from "./components/MenuItems";
+const { Header, Footer } = Layout;
+
+const styles = {
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "Roboto, sans-serif",
+    color: "#041836",
+    marginTop: "130px",
+    padding: "10px",
+  },
+  header: {
+    position: "fixed",
+    zIndex: 1,
+    width: "100%",
+    background: "#fff",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontFamily: "Roboto, sans-serif",
+    borderBottom: "2px solid rgba(0, 0, 0, 0.06)",
+    padding: "0 10px",
+    boxShadow: "0 1px 10px rgb(151 164 175 / 10%)",
+  },
+  headerRight: {
+    display: "flex",
+    gap: "20px",
+    alignItems: "center",
+    fontSize: "15px",
+    fontWeight: "600",
+  },
+};
+const App = ({ isServerInfo }) => {
+  const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
+    useMoralis();
+
+  useEffect(() => {
+    const connectorId = window.localStorage.getItem("connectorId");
+    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+      enableWeb3({ provider: connectorId });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, isWeb3Enabled]);
+
+  return (
+    <Layout style={{ height: "100vh", overflow: "auto" }}>
+      <Router>
+        <Header style={styles.header}>
+          <Logo />
+          <MenuItems />
+          <div style={styles.headerRight}>
+            <Chains />
+            <TokenPrice
+              address="0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c"
+              chain="bsc"
+              image="https://storageapi.fleek.co/72e9ae7a-4372-47c0-9426-4626e9765cdf-bucket/btc.png"
+              size="30px"
+            />
+            <NativeBalance />
+            <Account />
+          </div>
+        </Header>
+
+        <div style={styles.content}>
+          <Switch>
+            <Route exact path="/quickstart">
+              <QuickStart isServerInfo={isServerInfo} />
+            </Route>
+            <Route path="/wallet">
+              <Wallet />
+            </Route>
+            <Route path="/1inch">
+              <Tabs defaultActiveKey="1" style={{ alignItems: "center" }}>
+                <Tabs.TabPane tab={<span>Ethereum</span>} key="1">
+                  <DEX chain="eth" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Binance Smart Chain</span>} key="2">
+                  <DEX chain="bsc" />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={<span>Polygon</span>} key="3">
+                  <DEX chain="polygon" />
+                </Tabs.TabPane>
+              </Tabs>
+            </Route>
+            <Route path="/erc20balance">
+              <ERC20Balance />
+            </Route>
+            <Route path="/onramp">
+              <Ramper />
+            </Route>
+            <Route path="/erc20transfers">
+              <ERC20Transfers />
+            </Route>
+            <Route path="/nftBalance">
+              <NFTBalance />
+            </Route>
+            <Route path="/contract">
+              <Contract />
+            </Route>
+            <Route path="/nonauthenticated">
+              <>Please login using the "Authenticate" button</>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <Footer style={{ textAlign: "center" }}>
+        <Text style={{ display: "block" }}>
+          ⭐️ Mint your unique NFT{" "}
+          <a
+            href="https://mint.ligerinutoken.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mint NFT
+          </a>
+          , Your new PET in the METAVERSE
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          👉 Join our {""}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://t.me/ligerinuclub"
+          >
+            Telegram
+          </a>
+        </Text>
+
+        <Text style={{ display: "block" }}>
+          👉 Follow us on{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/ligerinuclub"
+          >
+            Twitter
+          </a>
+        </Text>
+      </Footer>
+    </Layout>
+  );
+};
+
+export const Logo = () => (
+  <div style={{ display: "flex" }}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="60"
+      width="40"
+      viewBox="-52.785 -88 457.47 528"
+    >
+      <path
+        d="M79.5 176l-39.7 39.7L0 176l39.7-39.7zM176 79.5l68.1 68.1 39.7-39.7L176 0 68.1 107.9l39.7 39.7zm136.2 56.8L272.5 176l39.7 39.7 39.7-39.7zM176 272.5l-68.1-68.1-39.7 39.7L176 352l107.8-107.9-39.7-39.7zm0-56.8l39.7-39.7-39.7-39.7-39.8 39.7z"
+        fill="#f0b90b"
+      />
+    </svg>
+  </div>
+);
+
+export default App;
